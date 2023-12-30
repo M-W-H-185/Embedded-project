ef8fad1cd0dd5faccbc742bfc7c58c9920d7229d
MaWenHao <377230216@qq.com>
2023-12-30 11:11:56 +0800

    OS：环形缓冲区完成

--- os_stc8h_test/main.c
+++ os_stc8h_test/main.c
@@ -93,8 +93,8 @@ void time0_handle(void)large reentrant
 // 消息队列
 typedef struct QueueHandle_t
 {
-	os_uint8_t *r_;		// 下一个要被读取的元素
-	os_uint8_t *w_;		// 下一个要被写入的位置
+	os_uint8_t *r_;			// 下一次要被读取的元素
+	os_uint8_t *w_;			// 下一次要被写入的位置
 	os_uint8_t 	length;		// 长度
 	os_uint8_t 	itemSize;	// 单个数据项的大小
 	os_uint8_t 	buff[6];
@@ -109,7 +109,7 @@ void queue_created(QueueHandle *queue){
 	queue->length = 6;
 	return;
 }
-// 队列写
+// 队列写 写入满不重新开始写，需要等待读完
 void queue_write(QueueHandle *queue, os_uint8_t _data)
 {	os_uint8_t *next_w = queue->w_ + 1;	// 下一次写入的位置
 	
@@ -131,33 +131,34 @@ void queue_write(QueueHandle *queue, os_uint8_t _data)
 	
 	return;
 }
-// 队列读
+// 队列读 读和写都到头了 重新回到数组0  ； 当前读和下次写位置重合表示空
 void queue_read(QueueHandle *queue, os_uint8_t *_data)
 {
 	os_uint8_t *next_r = queue->r_ + 1;	// 下一次读取的位置
-	
-	
-	
+
 	
 	// 读取到头了。代表读位置和写位置都到头了溢出了。两个一起滚回去 数组0
 	if( next_r >= &queue->buff[queue->length - 1] && ( queue->w_ >= &queue->buff[queue->length - 1] )    )
 	{
+		// 到了这里算是 环形缓冲区的最后一位的前一位数据  读取数据后 更新写入和读取指针 
+		*_data = *queue->r_;
+		
+	
 		queue->r_ = &queue->buff[0];	
 		queue->w_ = &queue->buff[0];	
 		// 返回溢出状态
 		return;
 	}
 	
-	
+	// 当前读取位置 和 下一次写入位置重合 表示空
+	if(queue->r_ == queue->w_)
+	{
+		return;
+	}
 	// 读取数据并更新头指针
 	*_data = *queue->r_;
 	queue->r_ = next_r;
 	
-	// 下一次读取的位置等于 写入 的位置 表示是空的
-	if(next_r == queue->w_)
-	{
-		return;
-	}
 
 
 
@@ -176,26 +177,42 @@ void main()
 	
 	// 初始化一个队列
 	queue_created(&queue_1);
+	// 写五次 是成功的
 	queue_write(&queue_1,1);
 	queue_write(&queue_1,2);
 	queue_write(&queue_1,3);
 	queue_write(&queue_1,4);
 	queue_write(&queue_1,5);
+	// 写五次 是成功的
 	
+	// 写五次后 失败
 	queue_write(&queue_1,6);
 	queue_write(&queue_1,7);	
-	
+	// 写五次后 失败
+
+	// 读五次 成功
 	queue_read(&queue_1, &test_data);
 	queue_read(&queue_1, &test_data);
 	queue_read(&queue_1, &test_data);
 	queue_read(&queue_1, &test_data);
 	queue_read(&queue_1, &test_data);
 	queue_read(&queue_1, &test_data);
-	
+	// 读五次 成功
+
+	// 读五次后 第一次读 失败并让 读和写指正回到数组0
 	queue_read(&queue_1, &test_data);
+	// 读五次后 第二次读 读和写重合  缓冲区数据为空
 	queue_read(&queue_1, &test_data);	
 
+
+	// 这两句后 缓冲区数据为空
+	
+	// 写一次成功
 	queue_write(&queue_1,0x33);
+	// 读一次成功
+	queue_read(&queue_1, &test_data);	
+	
+	// 这两句后 缓冲区数据为空
 
 	P0M0 = 0x00;   //设置P0.0~P0.7为双向口模式
 	P0M1 = 0x00;
